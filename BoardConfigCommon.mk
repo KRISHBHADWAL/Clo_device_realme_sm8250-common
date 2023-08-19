d132dab744ef5f3989a44a330b6fd70d437c416e
Krish Singh Bhadwal <75715914+KRISHBHADWAL@users.noreply.github.com>
2023-08-19 09:53:24 +0530

    sm8250-common: Setting KERNEL_CUSTOM_LLVM as false for P404
    As we dont need custom LLVM

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -84,7 +84,7 @@ BOARD_KERNEL_CMDLINE := \
     service_locator.enable=1 \
     swiotlb=2048
 
-KERNEL_CUSTOM_LLVM := true
+KERNEL_CUSTOM_LLVM := false
 
 BOARD_KERNEL_IMAGE_NAME := Image
 KERNEL_DEFCONFIG := vendor/sm8250_defconfig
